d738d350ec6c58152d253f16f32685312a248dc1
Johanna-hub <jyherman@gmail.com>
2017-11-24 11:50:48 +0000

    url clickable in title

--- public/dom.js
+++ public/dom.js
@@ -8,6 +8,7 @@ function onSubmit (event) {
   const search = inputfield.value;
   const searchTerm = search.split(" ").join("+");
   apiCall(searchTerm);
+  inputfield.value = "";
 }
 
 function apiCall(searchTerm) {
@@ -23,39 +24,18 @@ xhr.send();
 };
 
 
-function updateDom(resultObj){
-  console.log(resultObj);
-  var url = resultObj[0][0];
+function updateDom(resultObj) {
+  const url = resultObj[0][0];
   image.src = url;
-  var info = resultObj[0][1]
-  var listingUrl = resultObj[0][2];
-  var divElement = document.getElementById('listing');
-  while(divElement.firstChild){
+  const info = resultObj[0][1]
+  const listingUrl = resultObj[0][2];
+  const divElement = document.getElementById('listing');
+  while (divElement.firstChild) {
     divElement.removeChild(divElement.firstChild);
   }
-  var para = document.createElement('p');
-  var infotext = document.createTextNode(info);
+  const para = document.createElement('a');
+  const infotext = document.createTextNode(info);
   para.appendChild(infotext);
   divElement.appendChild(para);
-  var urlElement = document.getElementById('listingUrl');
-  while(urlElement.firstChild){
-    urlElement.removeChild(urlElement.firstChild)
-  }
-  var paraUrl = document.createElement('p');
-  var urltext = document.createTextNode(listingUrl);
-  paraUrl.appendChild(urltext);
-  urlElement.appendChild(paraUrl);
+  para.href = listingUrl;
 }
-
-
-// var xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function() {
-//   if (this.readyState == 4 && this.status == 200) {
-//     var name = JSON.parse(xhttp.responseText);
-//     var finalNames = name.result;
-//     updateDom(finalNames);
-//   }
-// };
-// xhttp.open("GET", "/endpoint" + "=" + inputtext , true);
-// xhttp.send(inputtext);
-// }, false);
